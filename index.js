dd9007cd4f8b70e33d33f3668eed8a643aadcd0a
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-04 17:48:35 +0100

    11. Assign secondCharacter to character

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ console.log(character);
 character = "'World'";
 console.log(character);
 let secondCharacter;
-console.log(secondCharacter);
 
-secondCharacter = "Test";
+console.log(secondCharacter);
+//change your secondCharacter assignment from "Test" to your character variable.
+secondCharacter = character;
+
 console.log(secondCharacter);
