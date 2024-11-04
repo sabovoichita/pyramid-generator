d4032f7d79d18c1f9fb3a9561188a3f30a6dfee7
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-04 18:27:31 +0100

    24. Declare var cities, console.log them

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ console.log(" ####### ");
 console.log("#########");
 let character = "'Hello'";
 let count = 8;
-
 let rows = ["Naomi", "Quincy", "CamperChan"];
-
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
 console.log(rows);
