497c6fc25c9ecff05452c792bda98bf6525cbd29
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-04 17:50:33 +0100

    12. Remove console logs

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-console.log("Let's build a pyramid using JavaScript basics");
+// console.log(character);
+// character = "'World'";
+// console.log(character);
+// let secondCharacter;
+// console.log(secondCharacter);
+// //change your secondCharacter assignment from "Test" to your character variable.
+// secondCharacter = character;
+// console.log(secondCharacter);
+
 console.log("    #    ");
 console.log("   ###   ");
 console.log("  #####  ");
 console.log(" ####### ");
 console.log("#########");
 let character = "'Hello'";
-console.log(character);
-character = "'World'";
-console.log(character);
-let secondCharacter;
-
-console.log(secondCharacter);
-//change your secondCharacter assignment from "Test" to your character variable.
-secondCharacter = character;
-
-console.log(secondCharacter);
