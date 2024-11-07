5a88f9eac5f912140036855803319d6f188170a7
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-07 15:02:21 +0100

    103. Replace your iteration statement with the correct statement using the subtraction assignment operator.

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function padRow(rowNumber, rowCount) {
   rows.push(padRow(rows.length + 1, count));
 }*/
 
-for (let i = count; i > 0; i = i - 1) {
+for (let i = count; i > 0; i -= 1) {
   rows.push(padRow(i, count));
 }
 
