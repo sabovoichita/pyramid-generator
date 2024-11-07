183dfe4af5e016efa042ccf1e899da0902b3390b
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-07 09:37:53 +0100

    60. Console.log("This works!") before and after return in the function

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ const rows = [];
 
 function padRow(name) {
   const test = "Testing";
+  console.log("This works!");
   return test;
+  console.log("This works!");
 }
 // console.log(test);
 const call = padRow("Voichita");
