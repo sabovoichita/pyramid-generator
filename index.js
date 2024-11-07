bf3856bf32f9712f8a75202e882d7c4481745d83
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-07 14:29:58 +0100

    96. Replaced the done reference in your padRow call with rows.length + 1.

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function padRow(rowNumber, rowCount) {
 let done = 0;
 while (rows.length < count) {
   done++;
-  rows.push(padRow(done, count));
+  rows.push(padRow(rows.length + 1, count));
 }
 
 let result = "";
